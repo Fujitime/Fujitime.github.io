3900af076958cc5e16eee35bc80f6d0aa81416bb
Fuji Halim Rabbani <104496391+Fujitime@users.noreply.github.com>
2023-01-10 17:52:09 +0700

    Update script.js
    nyoba dulu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,3 @@
-// navbar fixed
-
-window.onscroll = function(){
-    const header = document.querySelector('header')
-    const fixednav = header.offsetTop
-
-    if(window.pageYOffset > fixednav){
-        header.classList.add('navbar-fixed')
-    }
-        else{
-            header.classList.remove('navbar-fixed')
-        }
-    }
-
 // hamburger
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#nav-menu')
